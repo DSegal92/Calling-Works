593a775d10ceaf30d5117b3663911932790eb213
Daniel Segal <dsegal92@gmail.com>
2017-01-29 14:23:38 -0500

    Fix google api initializer spacing

--- config/initializers/google_api_key.rb
+++ config/initializers/google_api_key.rb
@@ -1,3 +1,3 @@
-GOOGLE_API_KEY =YAML.load_file(File.join(Rails.root,
-                                         'config',
-                                         'google_api.yml'))['key']
+GOOGLE_API_KEY = YAML.load_file(File.join(Rails.root,
+                                          'config',
+                                          'google_api.yml'))['key']
