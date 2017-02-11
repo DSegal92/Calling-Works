0124b2d0b32ff5cc4e621da85fb6bf57a778f439
Daniel Segal <DSegal92@gmail.com>
2017-02-11 13:41:49 -0500

    Retrieve and clear message from Redis

--- app/controllers/call_message_controller.rb
+++ app/controllers/call_message_controller.rb
@@ -2,7 +2,12 @@ class CallMessageController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def create
-    render xml: Gyoku.xml({ Response: { Say: params[:message],
+    call_key = params[:call_key]
+    message = Redis.new.get("CALL::#{call_key}")
+
+    Redis.new.del("CALL::#{call_key}")
+
+    render xml: Gyoku.xml({ Response: { Say: message,
                                         attributes!: { Say:
                                                        { voice: 'woman' } } } }, key_converter: :camelcase)
   end
