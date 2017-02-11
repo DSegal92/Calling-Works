42dcd92cd03d7af1f0df142e0fd5cf6f3d8524c2
Daniel Segal <DSegal92@gmail.com>
2017-02-11 16:33:09 -0500

    Fix call namespace

--- app/controllers/call_message_controller.rb
+++ app/controllers/call_message_controller.rb
@@ -2,7 +2,7 @@ class CallMessageController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def create
-    call_key = "CALL:#{params[:call_key]}"
+    call_key = "CALL::#{params[:call_key]}"
     message = REDIS.with { |r| r.get(call_key) }
 
     REDIS.with { |r| r.del(call_key) }
