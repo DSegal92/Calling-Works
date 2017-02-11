b9eb3d986550e02fe42c77839900dec662f56ea4
Daniel Segal <DSegal92@gmail.com>
2017-02-11 13:40:23 -0500

    Mount Sidekiq with basic auth protection

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  require 'sidekiq/web'
+  Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+    ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(username), ::Digest::SHA256.hexdigest(SIDEKIQ_CONFIG['username'])) &
+      ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password), ::Digest::SHA256.hexdigest(SIDEKIQ_CONFIG['password']))
+  end if Rails.env.production?
+  mount Sidekiq::Web, at: '/sidekiq'
+
   root 'call#new'
 
   resources :call_message, only: :create
