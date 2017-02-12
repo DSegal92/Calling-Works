83de8e4a6b9c49448e43e845b7ea16b91ac1d302
Daniel Segal <DSegal92@gmail.com>
2017-02-12 17:26:20 -0500

    Sanitize address when querying

--- lib/issues_adapter.rb
+++ lib/issues_adapter.rb
@@ -5,7 +5,7 @@ class IssuesAdapter
   base_uri 'https://5calls.org/issues'
 
   def self.issues(address)
-    resp = JSON.parse(get("/#{address}").body)['issues']
+    resp = JSON.parse(get("/#{URI.encode(address)}").body)['issues']
 
     resp.map { |x| Issue.new(x) }
   end
