32d1f2ed5e3f9258b2b069280d48155859ec9bad
Daniel Segal <DSegal92@gmail.com>
2017-02-12 21:53:31 -0500

    Do not retry failed jobs

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -4,6 +4,9 @@ SIDEKIQ_CONFIG = YAML.load_file(File.join(Rails.root,
 
 Sidekiq.configure_server do |config|
   config.redis = { password: REDIS_CONFIG['password'] }
+   config.server_middleware do |chain|
+      chain.add Sidekiq::Middleware::Server::RetryJobs, max_retries: 0
+   end
 end
 
 Sidekiq.configure_client do |config|
